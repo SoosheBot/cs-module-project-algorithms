86cc84515f87de6b6d9e5ebec2749aada29efcd0
sooshe <sooshe@sooshebot.com>
2020-06-11 14:07:38 -0500

    created first attempt at multiply l and r side of array -- test failed

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -1,16 +1,47 @@
 '''
 Input: a List of integers
 Returns: a List of integers
+Write a function that receives an array of integers and returns an array consisting of the product of all numbers in the array _except_ the number at that index. 
+
+For example, given 
+```
+[1, 7, 3, 4]
+```
+your function should return 
+```
+[84, 12, 28, 21]
+``` 
+by calculating 
+```
+[7*3*4, 1*3*4, 1*7*4, 1*7*3]
 '''
 def product_of_all_other_numbers(arr):
     # Your code here
+    #To-do--
+    # Loop through the array and when you land on a number remove it and multiply the rest of the numbers in the array, return that number to a new array
+    #maybe something where you multiply the right side of the array and the left side of the array and then combine those two into a final integer you put in a new array?
+    right = [0] * len(arr)
+    right[-1] = arr[-1]
 
-    pass
+    for i in range(1, len(arr)):
+        right[len(arr)-i-1] = right[len(arr)-i] * arr[len(arr)-i-1]
+        output = [0]*len(arr)
+        prefix = 1
+        current_index = 0
+        while current_index < len(output)-1:
+            output[current_index] = prefix * right[current_index+1]
+            prefix *= arr[current_index]
+            current_index +=1
+            output[-1] = prefix
+        return output
 
+    
+    
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    # arr = [1, 2, 3, 4, 5]
+    arr = [1, 2, 3, 4, 5]
     arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
+   
     print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
