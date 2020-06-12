f5c381423ca8fef71f6cb0036b115f62ba8854c6
sooshe <sooshe@sooshebot.com>
2020-06-12 18:47:23 -0500

    solve cookie_tin

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,10 +2,18 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
+def eating_cookies(n, cookie_tin=None):
 
-    pass
+    if cookie_tin == None:
+        cookie_tin = [0] * (n + 1)
+    if n <= 1:
+        cookie_tin[n] = 1
+    elif n == 2:
+        cookie_tin[n] = 2
+    elif cookie_tin[n] == 0:
+        cookie_tin[n] = eating_cookies(n - 1, cookie_tin) + eating_cookies(n - 2, cookie_tin) + eating_cookies(n - 3, cookie_tin)
+
+    return cookie_tin[n]
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
